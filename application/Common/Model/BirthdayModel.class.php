336e98b83a64e1659cf491605e1b977eed552f0a
xxg3053 <xxg3053@qq.com>
2015-11-23 22:08:30 +0800

    update birthday

--- application/Common/Model/BirthdayModel.class.php
+++ application/Common/Model/BirthdayModel.class.php
@@ -5,7 +5,8 @@ class BirthdayModel extends CommonModel{
 	//自动验证
 	protected $_validate = array(
 			//array(验证字段,验证规则,错误提示,验证条件,附加规则,验证时间)
-			array('user_name', 'require', '姓名不能为空！', 1, 'regex', CommonModel:: MODEL_BOTH )
+			array('user_name', 'require', '姓名不能为空！', 1, 'regex', CommonModel:: MODEL_BOTH ),
+			array('user_email','email','邮箱格式不正确！',0,'',CommonModel:: MODEL_BOTH )
 	);
 	
 	protected $_auto = array(
